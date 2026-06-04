1386dacda59cdb1db149f9f239caabb96d5f1778
Kaletts <100546535+Kaletts@users.noreply.github.com>
2026-06-04 14:58:54 +0200

    Actualizada fecha con minuto y segundos

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-function getTodayString() {
-      const now = new Date();
-      return String(now.getDate()).padStart(2, '0') +
-             String(now.getMonth() + 1).padStart(2, '0') +
-             now.getFullYear();
-    }
+      function getTodayString() {
+        const now = new Date();
+      
+        const day = String(now.getDate()).padStart(2, '0');
+        const month = String(now.getMonth() + 1).padStart(2, '0');
+        const year = now.getFullYear();
+      
+        const hours = String(now.getHours()).padStart(2, '0');
+        const minutes = String(now.getMinutes()).padStart(2, '0');
+        const seconds = String(now.getSeconds()).padStart(2, '0');
+      
+        return `${day}-${month}-${year}_${hours}${minutes}${seconds}`;
+      }
 
     function downloadXML(content, fileName) {
       const blob = new Blob([content], { type: 'application/xml' });
